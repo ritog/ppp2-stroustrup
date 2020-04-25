7f6da81e50557dfb0fd18119361f95d0fd006054
truth-r <coder.rg7@gmail.com>
2020-04-25 20:37:07 +0530

    sqrt cpp

---
+++ 3_Objects_Types_Values/3_Try_This/sqrt.cpp
@@ -0,0 +1,12 @@
+#include "../../std_lib_facilities.h"
+
+int main() {
+    cout << "Please enter a floating point value: ";
+    int n;
+    cin >> n;
+    double dn = n;
+    // double sqred = sqrt(dn);
+    cout << "\nsquare root of n ==" << sqrt(dn);
+
+    return 0;
+}
