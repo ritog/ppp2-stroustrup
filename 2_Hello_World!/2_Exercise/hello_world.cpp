51be3086a3981b26dd52331eace6223b989e439f
physrg <socnet.rg@gmail.com>
2020-04-25 19:58:17 +0530

    initial commit

---
+++ 2_Hello_World!/2_Exercise/hello_world.cpp
@@ -0,0 +1,11 @@
+#include "../../std_lib_facilities.h"
+
+int main() {
+    cout << "Hello, World!\nHere we go!\n";
+    keep_window_open(); //in line 170 of the header file, for Windows 
+
+    return 0;
+}
+
+
+
