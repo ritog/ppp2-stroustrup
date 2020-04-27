91ea47c2c00443574031b065d4c4a622edb6252e
physrg <socnet.rg@gmail.com>
2020-04-27 20:57:02 +0530

    4_Drill before vector

--- 4_Computation/4_Drill/4_Drill.cpp
+++ 4_Computation/4_Drill/4_Drill.cpp
@@ -53,22 +53,68 @@ int main() {
         }  
     }*/
 
+/*      
+UP TO DRILL 6
         double a = 0.0;
         double largest = 0.0;
-        double smallest = 0.0;
+        double smallest = 1.7e308; //upper bound of double type
     while (cin >> a) {
         cout << a << "\n";
         if (a > largest) {
             largest = a;
             cout << largest << " is the largest number so far.";
         }
-        smallest = a;
         if (a < smallest) {
             smallest = a;
             cout << smallest << " is the smallest number so far.";
         }
+    } */
+
+        double a = 0.0;
+        double largest = 0.0;
+        double smallest = 1.7e308;
+        string unit = "";
+        double sum = 0.0;
+        double cm_to_m = 0.01;
+        double in_to_m = 0.0254;
+        double ft_to_m = 0.3048;
+        int count = 0;
+
+    cout << "Enter a length with unit separated by a space.(in/ft/m/cm)\n";
+    cout << "Example: \"12 in\", \"15.6 ft\"\n";
+    while (cin >> a >> unit) {
+        if (unit == "in" || unit == "ft" || unit == "m" || unit == "cm") {
+            ++count;
+            cout << a << " " << unit << "\n";
+            if (unit == "in") {
+                sum += a * in_to_m;
+                if ((a * in_to_m) > largest) {largest = a * in_to_m;}
+                if ((a * in_to_m) < smallest) {smallest = a * in_to_m;}
+            }
+            else if (unit == "ft") {
+                sum += a * ft_to_m;
+                if ((a * ft_to_m) > largest) {largest = a * ft_to_m;}
+                if ((a * ft_to_m) < smallest) {smallest = a * ft_to_m;}
+            }
+            else if (unit == "cm") {
+                sum += a * cm_to_m;
+                if ((a * cm_to_m) > largest) {largest = a * cm_to_m;}
+                if ((a * cm_to_m) < smallest) {smallest = a * cm_to_m;}
+            }
+            else if (unit == "m") {
+                sum += a;
+                if (a > largest) {largest = a;}
+                if (a < smallest) {smallest = a;}
+            }
+        }
+        else {
+            cout << "Entered unit is not supported. Use a supported unit:\n";
+        }
     }
-
-
+    cout << "\nThe sum of values entered (in metres): "
+         << sum << "\n";
+    cout << "\nThe largest value (in metres) is: " << largest;
+    cout << "\nThe smallest value is (in metres) is: " << smallest;
+    cout << "\n";
     return 0;
 }
