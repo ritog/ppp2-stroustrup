480a673797789b43328f58a16009bd0ec80623e9
truth-r <coder.rg7@gmail.com>
2020-05-03 15:37:49 +0530

    eratosthenes

---
+++ 4_Computation/4_Exercise/13_eratosthenes.cpp
@@ -0,0 +1,20 @@
+#include "../../std_lib_facilities.h"
+
+int main() {
+    list <int> nums {1};
+    for (int i = 2; i <= 100; ++i) {nums.push_back(i);}
+
+    // for(auto x: nums) { cout << x << " ";}
+
+    // int p = 2;
+    for (int p = 2; p <= 10; ++p)
+    {nums.remove_if([p] (int i) {return !(i%p);});}
+
+    for(auto x: nums) { cout << x << " ";}
+
+    cout << "\n";
+
+
+    return 0;
+
+}
